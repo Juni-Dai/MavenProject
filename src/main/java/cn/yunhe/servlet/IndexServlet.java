121ceaee309b3d7a8200c9ba55a56ee0765caab0
Juni <juni_1101@163.com>
2019-09-29 09:14:42 +0800

    第一次提交

---
+++ src/main/java/cn/yunhe/servlet/IndexServlet.java
@@ -0,0 +1,33 @@
+package cn.yunhe.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class IndexServlet
+ */
+public class IndexServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public IndexServlet() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().print("hello maven");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
